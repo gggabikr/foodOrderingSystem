77ce7fbe9f549a45c421f33b5eb6f0af8bfc641a
Jason Sejik Lee <gggabikr@gmail.com>
2023-02-21 22:51:16 -0800

    Create MenuItReplicaApplication.java

---
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/MenuItReplicaApplication.java
@@ -0,0 +1,13 @@
+package com.menuit.menuitreplica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuItReplicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuItReplicaApplication.class, args);
+    }
+
+}
