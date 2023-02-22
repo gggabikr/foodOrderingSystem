867884b83f8da3bbe15c7a2c36fe6d8aea9b69c2
Jason Sejik Lee <gggabikr@gmail.com>
2023-02-21 22:50:50 -0800

    Create Address.java

---
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Address.java
@@ -0,0 +1,27 @@
+package com.menuit.menuitreplica.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String province;
+    private String city;
+    private String street;
+    private String unit;
+    private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String province ,String city, String street, String unit, String zipcode) {
+        this.province = province;
+        this.city = city;
+        this.street = street;
+        this.unit = unit;
+        this.zipcode = zipcode;
+    }
+}
