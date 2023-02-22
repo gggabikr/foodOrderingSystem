7ad1c6fdf7f5c10b63d9a8144d326fca6e63f132
Jason Sejik Lee <gggabikr@gmail.com>
2023-02-21 22:50:55 -0800

    Create Item.java

---
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Item.java
@@ -0,0 +1,49 @@
+package com.menuit.menuitreplica.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Getter @Setter
+public class Item {
+    @Id @GeneratedValue
+    @Column(name="item_id")
+    private Long id;
+
+    @NotEmpty(message = "Item name is mandatory.")
+    private String name;
+
+    private String description;
+
+    private String allergyInfo;
+
+    @NotEmpty(message = "Item price is mandatory.")
+    private float price;
+
+    private int discountPercent;
+
+    private int discountAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @NotEmpty(message = "Category is mandatory.")
+    private Category category;
+
+    private boolean ageRestriction; //1 = exist, 0 = not exist
+
+    private int calories;
+
+    private int minimumOrderCount;
+
+    @NotEmpty(message = "Item status is mandatory.")
+    private boolean status; // 1 = available, 0 = Out of order
+
+    @Enumerated(EnumType.STRING)
+    private ItemTag itemTags; //Popular, Best, Recommended, NoTag;
+
+    @NotEmpty(message = "Tax info is mandatory.")
+    private TaxInfo taxInfo; //NoTax, GST, PST, BOTH
+}
