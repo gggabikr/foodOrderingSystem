e3e34414191d08852119dcf2527efd7b573c3a19
Jason Sejik Lee <gggabikr@gmail.com>
2023-02-21 22:51:22 -0800

    Create MenuItReplicaApplicationTests.java

---
+++ MenuItReplica/src/test/java/com/menuit/menuitreplica/MenuItReplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menuit.menuitreplica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuItReplicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
