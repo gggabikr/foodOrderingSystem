88a023835aed667b1c66db4802882950cf82d925
Jason Sejik Lee <gggabikr@gmail.com>
2023-02-22 23:43:07 -0800

    change file name for favStoreList

---
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/FavStore.java
@@ -0,0 +1,22 @@
+package com.menuit.menuitreplica.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+public class FavStore {
+
+    @Id @GeneratedValue
+    @Column(name="favstore_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "favstore_id")
+    private Store favStore;
+}
