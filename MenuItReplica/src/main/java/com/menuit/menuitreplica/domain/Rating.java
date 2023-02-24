f9d1fdeddd688422fea5586919bf575554d9101d
Jason Sejik Lee <gggabikr@gmail.com>
2023-02-24 00:36:15 -0800

    Update Rating.java

--- MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Rating.java
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Rating.java
@@ -2,10 +2,12 @@ package com.menuit.menuitreplica.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
+@Table(uniqueConstraints=@UniqueConstraint(columnNames={"store_id", "user_id"}))
 @Entity
 @Getter @Setter
 public class Rating {
@@ -23,7 +25,27 @@ public class Rating {
     private User writtenBy;
 
     @NotEmpty(message = "rating is mandatory to leave a rating.")
-    private float rating; // 1~5 stars
+    private double rating; // 1~5 stars
 
     private String comment;
+
+    protected Rating() {}
+
+    //==Relational methods==//
+    public void setUser(User user){
+        this.writtenBy = user;
+        user.getRatings().add(this);
+    }
+
+    public void setStore(Store store){
+        this.store = store;
+        store.getRatings().add(this);
+    }
+
+    public Rating(User user, Store store, double rating, @Nullable String comment){
+        this.writtenBy = user;
+        this.store = store;
+        this.rating = rating;
+        this.comment = comment;
+    }
 }
