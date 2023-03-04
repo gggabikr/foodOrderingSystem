af4adafdec50207355c823f9db2fd62803fec016
Jason Sejik Lee <gggabikr@gmail.com>
2023-03-04 11:48:29 -0800

    Update OrderService.java

--- MenuItReplica/src/main/java/com/menuit/menuitreplica/service/OrderService.java
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/service/OrderService.java
@@ -8,6 +8,7 @@ import com.menuit.menuitreplica.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -49,7 +50,9 @@ public class OrderService {
         return order.getId();
     }
 
-    public void cancelOrder(Order order) throws IllegalAccessException {
+    public void cancelOrder(Long orderId) throws IllegalAccessException {
+        Order order = orderRepository.findOne(orderId);
+
         boolean noPaymentExist = true;
         for(Payer payer : order.getPayment().getPayers()){
             if (payer.isPaid()) {
@@ -77,27 +80,43 @@ public class OrderService {
         return orderRepository.findOneOrderItem(id);
     }
 
-    public List<Order> findByUser(User user){
+    public List<Order> findByUser(Long userId){
+        User user = userRepository.findOne(userId);
         return orderRepository.findByUser(user);
     }
 
-    public List<Order> findByStore(Store store){
+    public List<Order> findByStore(Long storeId){
+        Store store = storeRepository.findOne(storeId);
         return orderRepository.findByStore(store);
     }
 
-    public List<Order> findByItem(Item item){
+    public List<Order> findByItem(Long itemId){
+        Item item = itemRepository.findOne(itemId);
         return orderRepository.findByItem(item);
     }
 
-    public List<Order> findByOrderType(OrderType orderType){
+    public List<Order> findByOrderType(String str){
+        OrderType orderType = OrderType.valueOf(str);
         return orderRepository.findByOrderType(orderType);
     }
 
-    public List<Order> findByOrderStatus(OrderStatus orderStatus){
+    public List<Order> findByOrderStatus(String str){
+        OrderStatus orderStatus = OrderStatus.valueOf(str);
         return orderRepository.findByOrderStatus(orderStatus);
     }
 
-    public List<Order> findOrdersBetweenTwoTimes(Timestamp startTime, Timestamp endTime){
+    public List<Order> findOrdersBetweenTwoTimes(String start, String end){
+        //As I will use html input 'datetime-local', the inputs will be like "1985-04-12T23:20"
+        //which means there is no 'second' data.
+        // make sure the seconds are set before parsing
+        if (StringUtils.countOccurrencesOf(start, ":") == 1) {
+            start += ":00";
+        }
+        if (StringUtils.countOccurrencesOf(end, ":") == 1) {
+            end += ":00";
+        }
+        Timestamp startTime = Timestamp.valueOf(start.replace("T", " "));
+        Timestamp endTime = Timestamp.valueOf(end.replace("T", " "));
         return orderRepository.findOrdersBetweenTwoTimes(startTime, endTime);
     }
 
@@ -109,7 +128,9 @@ public class OrderService {
         return orderRepository.findByTotalTipAmount(startAmount, endAmount);
     }
 
-    public List<Order> findByUserAndStore(User user, Store store){
+    public List<Order> findByUserAndStore(Long userId, Long storeId){
+        User user = userRepository.findOne(userId);
+        Store store = storeRepository.findOne(storeId);
         return orderRepository.findByUserAndStore(user, store);
     }
 }
