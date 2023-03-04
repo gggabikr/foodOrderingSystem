8bb3d3bc181d5dea5f2d3faeb16516634113364f
Jason Sejik Lee <gggabikr@gmail.com>
2023-03-04 11:48:27 -0800

    Update RatingRepository.java

--- MenuItReplica/src/main/java/com/menuit/menuitreplica/repository/RatingRepository.java
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/repository/RatingRepository.java
@@ -1,10 +1,12 @@
 package com.menuit.menuitreplica.repository;
 import com.menuit.menuitreplica.domain.Rating;
 import com.menuit.menuitreplica.domain.Store;
+import com.menuit.menuitreplica.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,5 +21,41 @@ public class RatingRepository {
             em.persist(rating);
         }
         return rating.getId();
+    }
+
+    public Rating findById(Long id){
+        return em.find(Rating.class, id);
+    }
+
+    public List<Rating> findByUser(User user){
+        return em.createQuery("select r from Rating r where r.writtenBy = :user", Rating.class)
+                .setParameter("user", user)
+                .getResultList();
+    }
+
+    public List<Rating> findByStore(Store store){
+        return em.createQuery("select r from Rating r where r.store = :store", Rating.class)
+                .setParameter("store", store)
+                .getResultList();
+    }
+
+    public List<Rating> findByString(String str){
+        return em.createQuery(
+                        "SELECT r FROM Rating r WHERE UPPER(r.comment) LIKE CONCAT('%' ,UPPER(TRIM(:str)), '%')",Rating.class)
+                .setParameter("str", str)
+                .getResultList();
+    }
+
+    public List<Rating> findByScore(int score){
+        return em.createQuery("select r from Rating r where r.score = :score", Rating.class)
+                .setParameter("score", score)
+                .getResultList();
+    }
+
+    public List<Rating> findByUserAndStore(User user, Store store){
+        return em.createQuery("select r from Rating r where r.writtenBy = :user and r.store = :store and r.status = true", Rating.class)
+                .setParameter("store", store)
+                .setParameter("user", user)
+                .getResultList();
     }
 }
