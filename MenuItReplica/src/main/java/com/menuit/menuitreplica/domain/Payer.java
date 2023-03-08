8631f9302a8b008370a8b42533ee39529ff289eb
Jason Sejik Lee <gggabikr@gmail.com>
2023-03-07 23:08:42 -0800

    Update Payer.java

--- MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Payer.java
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Payer.java
@@ -160,11 +160,12 @@ public class Payer {
         return result;
     }
 
-
-    public void makeBill(){
-        System.out.println("===============Receipt=================");
+    public String makeBill(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("============== Receipt ================\n");
         Store store = this.getPayment().getOrder().getStore();
-        System.out.println("Store: "+ makeStringBlock(19, store.getName())+ "Order: " + makeStringBlockAtFront(6,this.getPayment().getOrder().getId()));
+        sb.append("Store: ").append(makeStringBlock(19, store.getName()))
+                .append("Order: ").append(makeStringBlockAtFront(6, this.getPayment().getOrder().getId())).append("\n");
 
         String type = "";
         OrderType orderType = this.getPayment().getOrder().getOrderType();
@@ -177,60 +178,152 @@ public class Payer {
         } else {
             type = "null";
         }
+        sb.append("Phone: ").append(store.getPhone())
+                .append("    Type: ").append(makeStringBlockAtFront(12, type)).append("\n");
 
-        System.out.println("Phone: " + store.getPhone() + "    Type: " + makeStringBlockAtFront(12, type));
         if(orderType == OrderType.table){
-            System.out.println("Table #: " + this.getPayment().getOrder().getUser().getFullName());
+            sb.append("Table #: ").append(this.getPayment().getOrder().getUser().getFullName()).append("\n");
         } else{
-            System.out.println(makeStringBlock(20,"Customer Name:") + makeStringBlock(19,this.getPayment().getOrder().getUser().getFullName()));
-            System.out.println("Customer Contact:   " + this.getPayment().getOrder().getUser().getPhone());
+            sb.append(makeStringBlock(20, "Customer Name:")).append(makeStringBlock(19, this.getPayment().getOrder().getUser().getFullName())).append("\n");
+            sb.append("Customer Contact:   ").append(this.getPayment().getOrder().getUser().getPhone()).append("\n");
         }
-        System.out.println("Address: " + store.getAddressString());
-        System.out.println("=======================================");
-        System.out.println("Item               Qnt   Unit     Price");
+        sb.append("Address: ").append(store.getAddressString()).append("\n");
+        sb.append("=======================================\n");
+        sb.append("Item               Qnt   Unit     Price\n");
         for(OrderItem orderItem: this.getOrderItems()){
-            System.out.println(makeStringBlock(20,orderItem.getItem().getName()) + makeStringBlock(5, orderItem.getCount()) + makeStringBlock(7, orderItem.getOrderPrice()) + makeStringBlockAtFront(7, ("$"+ orderItem.getTotalPrice())));
+            sb.append(makeStringBlock(20, orderItem.getItem().getName()))
+                    .append(makeStringBlock(5, orderItem.getCount()))
+                    .append(makeStringBlock(7, orderItem.getOrderPrice()))
+                    .append(makeStringBlockAtFront(7, ("$" + orderItem.getTotalPrice()))).append("\n");
         }
-        System.out.println("=======================================");
-        System.out.println("Subtotal:"+ makeStringBlockAtFront(30,("$"+ getSubtotal())));
+        sb.append("=======================================\n");
+        sb.append("Subtotal:").append(makeStringBlockAtFront(30, ("$" + getSubtotal()))).append("\n");
 
         if(this.additionalDiscount != 0 && this.discountReason.length()>5){
-            System.out.println(makeStringBlock(30,discountReason) + makeStringBlockAtFront(9, ("-$" + additionalDiscount)));
+            sb.append(makeStringBlock(30, discountReason)).append(makeStringBlockAtFront(9, ("-$" + additionalDiscount))).append("\n");
         }
+        sb.append("GST:").append(makeStringBlockAtFront(35, ("$" + getGST()))).append("\n");
+        sb.append("PST:").append(makeStringBlockAtFront(35, ("$" + getPST()))).append("\n");
+        sb.append("Total:").append(makeStringBlockAtFront(33, ("$" + getTotal()))).append("\n");
 
-        System.out.println("GST:"+makeStringBlockAtFront(35, ("$" + getGST())));
-        System.out.println("PST:"+makeStringBlockAtFront(35, ("$" + getPST())));
-        System.out.println("Total:"+makeStringBlockAtFront(33, ("$" + getTotal())));
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formatDateTime = now.format(formatter);
-        if(!this.getPayment().isGratuity()){
-            System.out.println("============Tip suggestions============");
-            System.out.println("13%: " + makeStringBlockAtFront(34,("$"+Math.round((getSubtotal()-getAdditionalDiscount())*13)/100.0)));
-            System.out.println("15%: " + makeStringBlockAtFront(34,("$"+Math.round((getSubtotal()-getAdditionalDiscount())*15)/100.0)));
-            System.out.println("18%: " + makeStringBlockAtFront(34,("$"+Math.round((getSubtotal()-getAdditionalDiscount())*18)/100.0)));
-            System.out.println("20%: " + makeStringBlockAtFront(34,("$"+Math.round((getSubtotal()-getAdditionalDiscount())*20)/100.0)));
-            System.out.println("==========" + formatDateTime + "==========");
-            System.out.println("==============Thank you!===============");
-        } else{
-            System.out.println("===========Gratuity applied============");
-            int gratuityPercent = getPayment().getGratuityPercent();
-            double gratuityAmount = Math.round(getSubtotal() * gratuityPercent) / 100.0;
-            System.out.println(makeStringBlock(19, gratuityPercent+"%:")+ makeStringBlockAtFront(20, "$"+gratuityAmount));
-            System.out.println("==========" + formatDateTime + "==========");
-            System.out.println("Amount due:"+ makeStringBlockAtFront(28, ("$"+Math.round((getTotal() + gratuityAmount)*100)/100.0)));
-            System.out.println("==============Thank you!===============");
-        }
+
         if(this.paid){
+            sb.append("Tip:").append(makeStringBlockAtFront(35, ("$" + getTipAmount()))).append("\n");
+            sb.append("========= Payment Information =========\n");
             if(this.paymentMethod != PaymentMethod.CASH){
-                System.out.println(this.paymentMethod.name() + ": 0000-****-0000-****"); // to be replaced with actual card number
+                sb.append(this.paymentMethod.name()).append(": 0000-****-0000-****\n"); // to be replaced with actual card number
             } else{
-                System.out.println(this.paymentMethod.name());
+                sb.append(this.paymentMethod.name()).append("\n");
             }
-            System.out.println("Paid amount: " + Math.round((this.total+ this.tipAmount)*100)/100.0);
+            sb.append("Paid amount: ").append(Math.round((this.total + this.tipAmount) * 100) / 100.0).append("\n");
+        } else{
+            if(!this.getPayment().isGratuity()){
+                sb.append("=========== Tip suggestions ===========\n");
+                sb.append("13%: ").append(makeStringBlockAtFront(34, ("$" + Math.round((getSubtotal() - getAdditionalDiscount()) * 13) / 100.0))).append("\n");
+                sb.append("15%: ").append(makeStringBlockAtFront(34, ("$" + Math.round((getSubtotal() - getAdditionalDiscount()) * 15) / 100.0))).append("\n");
+                sb.append("18%: ").append(makeStringBlockAtFront(34, ("$" + Math.round((getSubtotal() - getAdditionalDiscount()) * 18) / 100.0))).append("\n");
+                sb.append("20%: ").append(makeStringBlockAtFront(34, ("$" + Math.round((getSubtotal() - getAdditionalDiscount()) * 20) / 100.0))).append("\n");
+                sb.append("==========").append(formatDateTime).append("==========\n");
+            } else{
+                sb.append("========== Gratuity applied ===========\n");
+                int gratuityPercent = getPayment().getGratuityPercent();
+                double gratuityAmount = Math.round(getSubtotal() * gratuityPercent) / 100.0;
+                sb.append(makeStringBlock(19, gratuityPercent + "%:"))
+                        .append(makeStringBlockAtFront(20, "$" + gratuityAmount)).append("\n");
+                sb.append("==========").append(formatDateTime).append("==========\n");
+                sb.append("Amount due:").append(makeStringBlockAtFront(28, ("$" + Math.round((getTotal() + gratuityAmount) * 100) / 100.0))).append("\n");
+            }
         }
-        System.out.println();
+
+        sb.append("============= Thank you! ==============\n\n");
+
+        String toString = sb.toString();
+
+        //printing result string to see if it looks okay
+        System.out.println(toString);
+        return toString;
+
     }
+
+
+
+//    public void makeBill(){
+//        System.out.println("===============Receipt=================");
+//        Store store = this.getPayment().getOrder().getStore();
+//        System.out.println("Store: "+ makeStringBlock(19, store.getName())+ "Order: " + makeStringBlockAtFront(6,this.getPayment().getOrder().getId()));
+//
+//        String type = "";
+//        OrderType orderType = this.getPayment().getOrder().getOrderType();
+//        if(orderType.equals(OrderType.table)){
+//            type = "Dine-In";
+//        } else if(orderType.equals(OrderType.pickUp)){
+//            type = "Pick-Up";
+//        } else if(orderType.equals(OrderType.delivery)){
+//            type = "Delivery";
+//        } else {
+//            type = "null";
+//        }
+//
+//        System.out.println("Phone: " + store.getPhone() + "    Type: " + makeStringBlockAtFront(12, type));
+//        if(orderType == OrderType.table){
+//            System.out.println("Table #: " + this.getPayment().getOrder().getUser().getFullName());
+//        } else{
+//            System.out.println(makeStringBlock(20,"Customer Name:") + makeStringBlock(19,this.getPayment().getOrder().getUser().getFullName()));
+//            System.out.println("Customer Contact:   " + this.getPayment().getOrder().getUser().getPhone());
+//        }
+//        System.out.println("Address: " + store.getAddressString());
+//        System.out.println("=======================================");
+//        System.out.println("Item               Qnt   Unit     Price");
+//        for(OrderItem orderItem: this.getOrderItems()){
+//            System.out.println(makeStringBlock(20,orderItem.getItem().getName()) + makeStringBlock(5, orderItem.getCount()) + makeStringBlock(7, orderItem.getOrderPrice()) + makeStringBlockAtFront(7, ("$"+ orderItem.getTotalPrice())));
+//        }
+//        System.out.println("=======================================");
+//        System.out.println("Subtotal:"+ makeStringBlockAtFront(30,("$"+ getSubtotal())));
+//
+//        if(this.additionalDiscount != 0 && this.discountReason.length()>5){
+//            System.out.println(makeStringBlock(30,discountReason) + makeStringBlockAtFront(9, ("-$" + additionalDiscount)));
+//        }
+//
+//        System.out.println("GST:"+makeStringBlockAtFront(35, ("$" + getGST())));
+//        System.out.println("PST:"+makeStringBlockAtFront(35, ("$" + getPST())));
+//        System.out.println("Total:"+makeStringBlockAtFront(33, ("$" + getTotal())));
+//        LocalDateTime now = LocalDateTime.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        String formatDateTime = now.format(formatter);
+//
+//        if(this.paid){
+//            System.out.println("Tip:" + makeStringBlockAtFront(35, ("$" + getTipAmount())));
+//            System.out.println("==========Payment Information==========");
+//            if(this.paymentMethod != PaymentMethod.CASH){
+//                System.out.println(this.paymentMethod.name() + ": 0000-****-0000-****"); // to be replaced with actual card number
+//            } else{
+//                System.out.println(this.paymentMethod.name());
+//            }
+//            System.out.println("Paid amount: " + Math.round((this.total+ this.tipAmount)*100)/100.0);
+//        } else{
+//            if(!this.getPayment().isGratuity()){
+//                System.out.println("============Tip suggestions============");
+//                System.out.println("13%: " + makeStringBlockAtFront(34,("$"+Math.round((getSubtotal()-getAdditionalDiscount())*13)/100.0)));
+//                System.out.println("15%: " + makeStringBlockAtFront(34,("$"+Math.round((getSubtotal()-getAdditionalDiscount())*15)/100.0)));
+//                System.out.println("18%: " + makeStringBlockAtFront(34,("$"+Math.round((getSubtotal()-getAdditionalDiscount())*18)/100.0)));
+//                System.out.println("20%: " + makeStringBlockAtFront(34,("$"+Math.round((getSubtotal()-getAdditionalDiscount())*20)/100.0)));
+//                System.out.println("==========" + formatDateTime + "==========");
+//            } else{
+//                System.out.println("===========Gratuity applied============");
+//                int gratuityPercent = getPayment().getGratuityPercent();
+//                double gratuityAmount = Math.round(getSubtotal() * gratuityPercent) / 100.0;
+//                System.out.println(makeStringBlock(19, gratuityPercent+"%:")+ makeStringBlockAtFront(20, "$"+gratuityAmount));
+//                System.out.println("==========" + formatDateTime + "==========");
+//                System.out.println("Amount due:"+ makeStringBlockAtFront(28, ("$"+Math.round((getTotal() + gratuityAmount)*100)/100.0)));
+//            }
+//        }
+//        System.out.println("==============Thank you!===============");
+//        System.out.println();
+//        System.out.println();
+//    }
 
     public double tipPercentCalculator(double percentage){
         return Math.round(getSubtotal()*percentage)/100.0;
