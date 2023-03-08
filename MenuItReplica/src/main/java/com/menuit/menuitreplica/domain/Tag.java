b5fcf1bfdd9d12d7b3f760015f1632904736be2a
Jason Sejik Lee <gggabikr@gmail.com>
2023-03-07 20:38:44 -0800

    Update Tag.java

--- MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Tag.java
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Tag.java
@@ -1,7 +1,6 @@
 package com.menuit.menuitreplica.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Getter @Setter
+@Getter
 public class Tag {
     @Id @GeneratedValue
     @Column(name="tag_id")
@@ -17,4 +16,7 @@ public class Tag {
 
     private String name;
 
+    public void setName(String name){
+        this.name = name;
+    }
 }
