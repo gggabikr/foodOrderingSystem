e19e6656856e2600098336ec7d76d90688ed3b6c
Jason Sejik Lee <gggabikr@gmail.com>
2023-03-12 23:47:58 -0700

    Update Hours.java

--- MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Hours.java
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Hours.java
@@ -1,17 +1,19 @@
 package com.menuit.menuitreplica.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
-import java.sql.Time;
 import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
-@Getter @Setter
+@Getter
 public class Hours {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="hours_id")
     private Long id;
 
@@ -25,15 +27,79 @@ public class Hours {
     //Monday, Tuesday, Wednesday, Thursday,
     //Friday, Saturday, Sunday
 
-    private Time opening;
+    @Column(name = "opening_time")
+    private LocalTime openingTime;
 
-    private Time closing;
+    @Column(name = "closing_time")
+    private LocalTime closingTime;
+
+    @Column(name = "last_call")
+    private LocalTime lastCallTime;
 
     protected Hours(){};
 
-    public Hours(DayOfWeek day, Time opening, Time closing){
+    public Hours(DayOfWeek day, LocalTime opening, LocalTime closing, LocalTime lastCall){
+    //check if opening is before both closing and lastCall, and if closing is after lastCall
+
+        boolean flag1 = (lastCall.isBefore(closing) || lastCall.equals(closing) || closing.equals(LocalTime.MIDNIGHT));
+        boolean flag2 = ((closing.isAfter(opening) || closing.equals(opening)) || closing.equals(LocalTime.MIDNIGHT) && opening.isBefore(closing));
+        boolean flag3 = (lastCall.isAfter(opening) || lastCall.equals(opening) || lastCall.equals(closing));
+
+        if (!flag1 && !flag2 && !flag3){
+            throw new IllegalArgumentException("Opening time needs to be earliest, " +
+                    "closing time needs to be latest " +
+                    "and last call time needs to be between the two.");
+        }
         this.dayOfWeek = day;
-        this.opening = opening;
-        this.closing = closing;
+        this.openingTime = opening;
+        this.closingTime = closing;
+        this.lastCallTime = lastCall;
     }
+
+    public void setStore(Store store){
+        this.store = store;
+    }
+
+    public List<Hours> duplicateHour(Hours hour) {
+        // Create new Hours objects for all other days of the week
+        Store store = hour.getStore();
+
+        List<Hours> hoursList = new ArrayList<>();
+        for (DayOfWeek day : DayOfWeek.values()) {
+            if (day != this.dayOfWeek) {
+                Hours newHours = new Hours(day, this.openingTime, this.closingTime, this.lastCallTime);
+                newHours.setStore(store);
+                hoursList.add(newHours);
+            }
+        }
+
+        // Remove existing Hours objects with the same dayOfWeek value
+        List<Hours> existingHours = store.getOpenHours().stream()
+                .filter(h -> h.getDayOfWeek() != this.dayOfWeek)
+                .collect(Collectors.toList());
+        store.getOpenHours().removeAll(existingHours);
+
+        // Add the new Hours objects to the store's list of hours
+        store.getOpenHours().addAll(hoursList);
+
+        return hoursList;
+    }
+
+    public List<Hours> duplicateHourForSelectedDays(Hours hour, DayOfWeek... dayOfWeeks) {
+        Store store = hour.getStore();
+        List<Hours> hoursList = new ArrayList<>();
+
+        for (DayOfWeek day : dayOfWeeks) {
+            List<Hours> existingHours = store.getOpenHours().stream()
+                    .filter(h -> h.getDayOfWeek() == day)
+                    .collect(Collectors.toList());
+            store.getOpenHours().removeAll(existingHours);
+            Hours newHours = new Hours(day, hour.getOpeningTime(), hour.getClosingTime(), hour.getLastCallTime());
+            newHours.setStore(store);
+            store.getOpenHours().add(newHours);
+            hoursList.add(newHours);
+        }
+        return hoursList;
+    }
+
 }
