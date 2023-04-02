d91b210306ca57f884b06e63a96c3eb47938c789
Jason Sejik Lee <gggabikr@gmail.com>
2023-04-01 22:57:37 -0700

    Update Hours.java

--- MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Hours.java
+++ MenuItReplica/src/main/java/com/menuit/menuitreplica/domain/Hours.java
@@ -41,15 +41,32 @@ public class Hours {
     public Hours(DayOfWeek day, LocalTime opening, LocalTime closing, LocalTime lastCall){
     //check if opening is before both closing and lastCall, and if closing is after lastCall
 
-        boolean flag1 = (lastCall.isBefore(closing) || lastCall.equals(closing) || closing.equals(LocalTime.MIDNIGHT));
-        boolean flag2 = ((closing.isAfter(opening) || closing.equals(opening)) || closing.equals(LocalTime.MIDNIGHT) && opening.isBefore(closing));
-        boolean flag3 = (lastCall.isAfter(opening) || lastCall.equals(opening) || lastCall.equals(closing));
+        //TODO: 컨디션 세팅 다시하거나, 아예 하지말자.
+        //1. 오전에 열어서 오후에 닫음, 둘 사이에 라스트콜
+        //2. 아무때나 열어서 다음날 새벽에 닫음. 둘 사이에 라스트콜
+        //3. 아무때나 열어서 자정에 닫음. 라스트콜은 자정 전
+        //4. 아무때나 열어서 다음날 새벽에 닫음. 라스트콜이 자정
+        //5. 자정에 열어서 오전에 닫음. 라스트콜은 이른새벽
+        //까지가 통과 되어야 하는 케이스들.
+        //어떤 이유에서든 라스트콜이 둘 사이에 있지않거나,
+        //혹은 오픈시간이 클로즈보다 4시간 미만으로 많을때는 오류있어야...
+        //오픈 7시 클로즈 2시와 같이 단순히 보기엔 오픈이 클로즈시간보다 늦은시간처럼 보이는 스토어도 그런 스토어도 존재할수있다.
+//
+//        boolean flag = (lastCall.isAfter(opening) || lastCall.equals(opening) || lastCall.equals(closing))
+//                && ((closing.isAfter(opening) || closing.equals(opening)) || closing.equals(LocalTime.MIDNIGHT) && opening.isBefore(closing))
+//                && (lastCall.isBefore(closing) || lastCall.equals(closing) || closing.equals(LocalTime.MIDNIGHT));
+//
 
-        if (!flag1 && !flag2 && !flag3){
-            throw new IllegalArgumentException("Opening time needs to be earliest, " +
-                    "closing time needs to be latest " +
-                    "and last call time needs to be between the two.");
-        }
+//        boolean flag1 = (lastCall.isBefore(closing) || lastCall.equals(closing) || closing.equals(LocalTime.MIDNIGHT));
+//        boolean flag2 = ((closing.isAfter(opening) || closing.equals(opening)) || closing.equals(LocalTime.MIDNIGHT) && opening.isBefore(closing));
+//        boolean flag3 = (lastCall.isAfter(opening) || lastCall.equals(opening) || lastCall.equals(closing));
+
+//        if (!flag1 || !flag2 || !flag3){
+//        if(!flag){
+//            throw new IllegalArgumentException("Opening time needs to be earliest, " +
+//                    "closing time needs to be latest " +
+//                    "and last call time needs to be between the two.");
+//        }
         this.dayOfWeek = day;
         this.openingTime = opening;
         this.closingTime = closing;
@@ -88,17 +105,20 @@ public class Hours {
     public List<Hours> duplicateHourForSelectedDays(Hours hour, DayOfWeek... dayOfWeeks) {
         Store store = hour.getStore();
         List<Hours> hoursList = new ArrayList<>();
-
         for (DayOfWeek day : dayOfWeeks) {
             List<Hours> existingHours = store.getOpenHours().stream()
                     .filter(h -> h.getDayOfWeek() == day)
                     .collect(Collectors.toList());
             store.getOpenHours().removeAll(existingHours);
+            for (Hours hour2: existingHours){
+                hour2.setStore(null);
+            }
+
             Hours newHours = new Hours(day, hour.getOpeningTime(), hour.getClosingTime(), hour.getLastCallTime());
             newHours.setStore(store);
-            store.getOpenHours().add(newHours);
             hoursList.add(newHours);
         }
+        store.getOpenHours().addAll(hoursList);
         return hoursList;
     }
 
