e5fe39a2d89c3cd2974d32757638fe584fbc3c1d
Jason Sejik Lee <gggabikr@gmail.com>
2023-04-09 18:08:25 -0700

    Update HoursServiceTest.java

--- MenuItReplica/src/test/java/com/menuit/menuitreplica/service/HoursServiceTest.java
+++ MenuItReplica/src/test/java/com/menuit/menuitreplica/service/HoursServiceTest.java
@@ -1,10 +1,30 @@
 package com.menuit.menuitreplica.service;
 
+import com.menuit.menuitreplica.repository.OrderRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class HoursServiceTest {
+
+    @Autowired
+    StoreService storeService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    ItemService itemService;
+    @Autowired
+    HoursService hoursService;
 
     @Test
     public void findOne() {
