839289bd7b885b1efb9d7007253f59ea63e11fe0
Jason Sejik Lee <gggabikr@gmail.com>
2023-04-09 17:42:36 -0700

    Create HoursServiceTest.java

---
+++ MenuItReplica/src/test/java/com/menuit/menuitreplica/service/HoursServiceTest.java
@@ -0,0 +1,40 @@
+package com.menuit.menuitreplica.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HoursServiceTest {
+
+    @Test
+    public void findOne() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findOneByStoreAndDay() {
+    }
+
+    @Test
+    public void createHour() {
+    }
+
+    @Test
+    public void duplicateHourForAllDays() {
+    }
+
+    @Test
+    public void duplicateHourForSelectedDays() {
+    }
+
+    @Test
+    public void setOpenHoursForAllDays() {
+    }
+
+    @Test
+    public void findByStore() {
+    }
+}
