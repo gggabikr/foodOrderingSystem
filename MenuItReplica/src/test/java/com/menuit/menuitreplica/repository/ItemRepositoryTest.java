2c7d67fc408ee5e074ca114a5f94e46c491c234b
Jason Sejik Lee <gggabikr@gmail.com>
2023-04-23 08:54:41 -0700

    Create ItemRepositoryTest.java

---
+++ MenuItReplica/src/test/java/com/menuit/menuitreplica/repository/ItemRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.menuit.menuitreplica.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemRepositoryTest {
+
+    @Test
+    public void registerItem() {
+    }
+
+    @Test
+    public void removeItem() {
+    }
+
+    @Test
+    public void findOne() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findByStore() {
+    }
+
+    @Test
+    public void findByStoreDeleted() {
+    }
+
+    @Test
+    public void findByStoreAndCategory() {
+    }
+
+    @Test
+    public void findByStoreAndCategoryDeleted() {
+    }
+
+    @Test
+    public void findByItemType() {
+    }
+
+    @Test
+    public void findByStoreAndItemType() {
+    }
+
+    @Test
+    public void findByStoreAndItemTypeDeleted() {
+    }
+
+    @Test
+    public void findByItemTag() {
+    }
+
+    @Test
+    public void findByStatus() {
+    }
+
+    @Test
+    public void findDiscountedItemOnStore() {
+    }
+}
