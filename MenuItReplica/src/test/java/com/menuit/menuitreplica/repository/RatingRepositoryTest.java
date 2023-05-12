42e19982e3ab00ba0f9b9ab7170a53cb46ffa6fe
Jason Sejik Lee <gggabikr@gmail.com>
2023-05-12 00:16:24 -0700

    Create RatingRepositoryTest.java

---
+++ MenuItReplica/src/test/java/com/menuit/menuitreplica/repository/RatingRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.menuit.menuitreplica.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RatingRepositoryTest {
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void findByUser() {
+    }
+
+    @Test
+    public void findByStore() {
+    }
+
+    @Test
+    public void findByString() {
+    }
+
+    @Test
+    public void findByScore() {
+    }
+
+    @Test
+    public void findByUserAndStore() {
+    }
+}
